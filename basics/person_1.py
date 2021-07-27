d80bda337a24a44e0f55c7ae6a89cb3053d5812a
Jeremiah Malina <jmalina327@gmail.com>
2021-07-27 13:22:55 -0400

    Update bases example 1 to show equality comparison

--- basics/person_1.py
+++ basics/person_1.py
@@ -15,10 +15,18 @@ class Person:
 
 
 if __name__ == '__main__':
-    me = Person(
+    me: Person = Person(
         "Jeremiah", "Malina",
         "386 Park Ave S", "6th Floor",
         "New York", "New York", "10016",
         "United States of America"
     )
     print(me)
+
+    me2 = Person(
+        "Jeremiah", "Malina",
+        "386 Park Ave S", "6th Floor",
+        "New York", "New York", "10016",
+        "United States of America"
+    )
+    print(me == me2)
